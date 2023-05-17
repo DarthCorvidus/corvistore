e45baf66f29d8282e4285d85495b7d10e8924708
DarthCorvidus <github@vm01.telton.de>
2023-05-17 13:55:01 +0200

    Set CPAdm database to the default path (for now).

--- src/cpadm.php
+++ src/cpadm.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 require_once __DIR__."/../vendor/autoload.php";
-$databasePath = dirname($_SERVER["SCRIPT_FILENAME"])."/crow-protect.sqlite";
+$databasePath = "/var/lib/crow-protect/crow-protect.sqlite";
 $shared = new Shared();
 $shared->useSQLite($databasePath);
 $adm = new CPAdm($shared->getEPDO());
