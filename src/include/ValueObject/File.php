7fde3838da091e05fa59ea59a6ae70e690507e5c
DarthCorvidus <github@vm01.telton.de>
2023-05-28 12:56:47 +0200

    Some tweaks on File class. Performance went up to 1.33 seconds from 2.77 seconds for 91.445 instantiations.

--- src/include/ValueObject/File.php
+++ src/include/ValueObject/File.php
@@ -9,26 +9,54 @@ class File {
 	private $owner;
 	private $group;
 	private $type;
+	private static $userCache = array();
+	private static $groupCache = array();
 	function __construct(string $path) {
-		$this->path = realpath($path);
+		// removed realpath() here, as I don't see how it could be called with a
+		// relative path, apart from any initial path supplied by pesky users,
+		// on which realpath() can be called once.
+		$this->path = $path;
 		$stat = stat($this->path);
 		$this->ctime = $stat["ctime"];
 		$this->atime = $stat["atime"];
 		$this->mtime = $stat["mtime"];
 		$this->permissions = fileperms($path);
-		
-		$owner = posix_getpwuid($stat["uid"]);
-		$group = posix_getgrgid($stat["gid"]);
-		$this->owner = $owner["name"];
-		$this->group = $group["name"];
+		// group & user names are cached
+		$this->owner = $this->getOwnerName($stat["uid"]);
+		$this->group = $this->getGroupName($stat["gid"]); 
 		$this->size = filesize($path);
 		$this->type = Catalog::TYPE_OTHER;
+		//usually, most files are files, so check for file first and end early.
+		if(is_file($this->path)) {
+			$this->type = Catalog::TYPE_FILE;
+			return;
+		}
 		if(is_dir($this->path)) {
 			$this->type = Catalog::TYPE_DIR;
 		}
-		if(is_file($this->path)) {
-			$this->type = Catalog::TYPE_FILE;
+	}
+	
+	private function getOwnerName(int $uid) {
+		if(isset(self::$userCache[$uid])) {
+			return self::$userCache[$uid];
 		}
+		$owner = posix_getpwuid($uid);
+		self::$userCache[$uid] = $owner["name"];
+	return $owner["name"];
+	}
+	
+	private function getGroupName(int $gid) {
+		if(isset(self::$groupCache[$gid])) {
+			return self::$groupCache[$gid];
+		}
+		$group = posix_getgrgid($gid);
+		self::$groupCache[$gid] = $group["name"];
+	return $group["name"];
+	}
+	
+	
+	function isEqual(CatalogEntry $entry) {
+		
 	}
 	
 	function getPath(): string {
