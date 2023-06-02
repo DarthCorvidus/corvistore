2a1b5dbd8f69f3a0f4fffa67bc48e8ff08edc89b
DarthCorvidus <github@vm01.telton.de>
2023-06-02 16:53:30 +0200

    Added an empty array to new CPAdm() in TestHelper (which would be $argv).

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -55,7 +55,7 @@ class TestHelper {
 	}
 	
 	static function initServer() {
-		$cpadm = new CPAdm(TestHelper::getEPDO());
+		$cpadm = new CPAdm(TestHelper::getEPDO(), array());
 		$cpadm->handleCommand(new CommandParser("define storage basic01 type=basic location=".__DIR__."/storage/basic01/"));
 		$cpadm->handleCommand(new CommandParser("define partition backup-main type=common storage=basic01"));
 		$cpadm->handleCommand(new CommandParser("define policy forever partition=backup-main"));
