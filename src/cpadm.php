301eb29d47d1898cbea3a7a963c02a0c1c2a2e7e
DarthCorvidus <github@vm01.telton.de>
2023-07-01 18:17:42 +0200

    Removed PDO from cpadm.

--- src/cpadm.php
+++ src/cpadm.php
@@ -1,8 +1,5 @@
 #!/usr/bin/env php
 <?php
 require_once __DIR__."/../vendor/autoload.php";
-$databasePath = "/var/lib/crow-protect/crow-protect.sqlite";
-$shared = new Shared();
-$shared->useSQLite($databasePath);
 $adm = new \Admin\Client($argv);
 $adm->run();
