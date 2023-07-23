b4b56d425fdfdea61328fd18b2c813de7bb56091
DarthCorvidus <github@vm01.telton.de>
2023-07-23 16:42:10 +0200

    Added "onConnect" to StreamHubListener.

--- src/include/Net/StreamHubListener.php
+++ src/include/Net/StreamHubListener.php
@@ -2,4 +2,5 @@
 interface StreamHubListener {
 	function onRead(string $name, int $id, $stream);
 	function onWrite(string $name, int $id, $stream);
+	function onConnect(string $name, int $id, $newClient);
 }
