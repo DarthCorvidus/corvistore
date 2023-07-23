7475fc597f8e5f352dc0c93ff4008456bacb7f72
DarthCorvidus <github@vm01.telton.de>
2023-07-23 16:44:11 +0200

    Added connection handling to StreamHub.

--- src/include/Net/StreamHub.php
+++ src/include/Net/StreamHub.php
@@ -1,14 +1,16 @@
 <?php
 class StreamHub {
-	private $server;
-	private $clients;
+	private $server = array();
+	private $clients = array();
 	private $streamHubListeners;
+	private $counters = array();
 	function __construct() {
 		;
 	}
 	
 	function addServer(string $name, $stream) {
 		$this->server[$name] = $stream;
+		$this->counters[$name] = 0;
 	}
 	
 	function addCustomStream(string $name, $stream) {
@@ -30,6 +32,9 @@ class StreamHub {
 			foreach($this->clients as $key => $value) {
 				$read[$key] = $value;
 			}
+			foreach($this->server as $key => $value) {
+				$read[$key] = $value;
+			}
 			if(@stream_select($read, $write, $except, $tv_sec = 5) < 1) {
 				#$error = socket_last_error($this->socket);
 				#if($error!==0) {
@@ -38,6 +43,15 @@ class StreamHub {
 				continue;
 			}
 			foreach($read as $key => $value) {
+				if(in_array($key, array_keys($this->server), TRUE)) {
+					$client = stream_socket_accept($value);
+					$next = $this->counters[$key];
+					$this->counters[$key]++;
+					$this->clients[$key.":".$next] = $client;
+					$this->streamHubListeners[$key]->onConnect($key, $next, $client);
+					continue;
+				}
+				
 				$exp = explode(":", $key);
 				$this->streamHubListeners[$exp[0]]->onRead($exp[0], (int)$exp[1], $value);
 			}
