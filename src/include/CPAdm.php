411acfbbb07e816963a3f25787cfa1e6e98ece39
DarthCorvidus <github@vm01.telton.de>
2023-08-20 17:22:29 +0200

    CPAdm should not be needed anymore, but it is used in tests. I should replace and remove it at some point.

--- src/include/CPAdm.php
+++ src/include/CPAdm.php
@@ -51,6 +51,52 @@ class CPAdm {
 			$query->run();
 			return;
 		}
+		if($command->getCommand()=="md5") {
+			if($command->getObject()=="") {
+				return;
+			}
+			$storage = Storage::fromName($this->pdo, $command->getObject());
+			$stmt = $this->pdo->prepare("select * from n_version2basic where dst_id = ?");
+			$stmt->execute(array($storage->getId()));
+			foreach($stmt as $key => $value) {
+				$path = $storage->getPathForIdFile($value["nvb_id"]);
+				echo $path.PHP_EOL;
+				echo md5_file($path).PHP_EOL;
+			}
+		return;
+		}
+		if($command->getCommand()=="backup") {
+			if($command->getObject()!="partition") {
+				return;
+			}
+			$model = new CPModelBackup($this->pdo);
+			$command->import($model);
+			$sourcePartition = Partition::fromName($this->pdo, $command->getPositional(0));
+			$targetPartition = $sourcePartition->getCopyPartition();
+			$sourceStorage = Storage::fromId($this->pdo, $sourcePartition->getStorageId());
+			$targetStorage = Storage::fromId($this->pdo, $targetPartition->getStorageId());
+			$versionsSource = $sourceStorage->getStoredVersions($sourcePartition);
+			$versionsTarget = $targetStorage->getStoredVersions($targetPartition);
+			$diff = array_diff($versionsSource, $versionsTarget);
+			$max = count($diff);
+			$i=0;
+			$this->pdo->beginTransaction();
+			foreach($diff as $key => $value) {
+				if($i%100==0) {
+					$this->pdo->commit();
+					$this->pdo->beginTransaction();
+				}
+				if($i%1000==0) {
+					echo "Copied ".$i." of ".$max.PHP_EOL;
+					
+				}
+				$sourceHandle = $sourceStorage->getReadHandle($sourcePartition, $value);
+				$targetStorage->storeFromHandle($targetPartition, $value, $sourceHandle);
+				$i++;
+			}
+			$this->pdo->commit();
+		return;
+		}
 		throw new InvalidArgumentException("Invalid command '".$command->getCommand()."'.");
 	}
 	
