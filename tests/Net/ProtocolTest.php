339160f4e25b0404949c265be1853e45431ff4d7
DarthCorvidus <github@vm01.telton.de>
2023-09-09 20:30:48 +0200

    Added tests to ProtocolTests which check for the correct interoperability of ProtocolAsync and ProtocolSync.

--- tests/Net/ProtocolTest.php
+++ tests/Net/ProtocolTest.php
@@ -2,12 +2,16 @@
 declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use Net\Protocol;
-class ProtocolTest extends TestCase {
+class ProtocolTest extends TestCase implements \Net\ProtocolAsyncListener {
 	const FILE_SIZE = 17201;
+	private $lastString;
+	private $lastSerialized;
 	function tearDown() {
 		if(file_exists(self::getExamplePath())) {
 			unlink(self::getExamplePath());
 		}
+		$this->lastSerialized = NULL;
+		$this->lastString = NULL;
 	}
 	
 	static function getExamplePath(): string {
@@ -43,4 +47,79 @@ class ProtocolTest extends TestCase {
 		$this->expectExceptionMessage("malformed control block, 84 does not equal 46");
 		Protocol::determineControlBlock($block);
 	}
+	
+	function testAsyncSyncSendString() {
+		$sf = new StreamFake("");
+		$async = new Net\ProtocolAsync($this);
+		$sync = new Net\ProtocolSync($sf);
+		
+		$async->sendCommand("quit");
+		while($async->hasWrite()) {
+			$sf->write($async->onWrite());
+			$async->onWritten();
+		}
+		$command = $sync->getCommand();
+		$this->assertEquals("quit", $command);
+	}
+
+	function testSyncAsyncSendString() {
+		$sf = new StreamFake("");
+		$async = new Net\ProtocolAsync($this);
+		$sync = new Net\ProtocolSync($sf);
+		
+		$sync->sendCommand("quit");
+		while(!$sf->eof()) {
+			$async->onRead($sf->read(1024));
+		}
+		$this->assertEquals("quit", $this->lastString);
+	}
+
+	function testAsyncSyncSendSerialized() {
+		$expected = $_SERVER;
+		$sf = new StreamFake("");
+		$async = new Net\ProtocolAsync($this);
+		$sync = new Net\ProtocolSync($sf);
+		
+		$async->sendSerialize($expected);
+		while($async->hasWrite()) {
+			$sf->write($async->onWrite());
+			$async->onWritten();
+		}
+		$unserialized = $sync->getSerialized();
+		$this->assertEquals($expected, $unserialized);
+	}
+
+	function testSyncAsyncSendSerialized() {
+		$expected = $_SERVER;
+		$sf = new StreamFake("");
+		$async = new Net\ProtocolAsync($this);
+		$sync = new Net\ProtocolSync($sf);
+		
+		$sync->sendSerialize($expected);
+		while(!$sf->eof()) {
+			$async->onRead($sf->read(1024));
+		}
+		$this->assertEquals($expected, $this->lastSerialized);
+	}
+	
+	public function onCommand(\Net\ProtocolAsync $protocol, string $command) {
+		$this->lastString = $command;
+	}
+
+	public function onDisconnect(\Net\ProtocolAsync $protocol) {
+		
+	}
+
+	public function onMessage(\Net\ProtocolAsync $protocol, string $message) {
+		
+	}
+
+	public function onOk(\Net\ProtocolAsync $protocol) {
+		
+	}
+
+	public function onSerialized(\Net\ProtocolAsync $protocol, $unserialized) {
+		$this->lastSerialized = $unserialized;
+	}
+
 }
