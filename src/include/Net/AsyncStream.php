c3f51b907f783e7b5c3282f894240385f0412cd1
Claus-Christoph Küthe <github@vm01.telton.de>
2024-11-05 17:13:11 +0100

    More generic version of \Server\TaskClient.
    TaskServer was just meant to see 'if it works'. AsyncStream is more generic
    and resilient, with fread only triggering AsyncProtocol::onRead if there was
    some data.
    Protocol has to be set separatedly, as I need to give the instance of
    TaskClient to the protocol listener.

---
+++ src/include/Net/AsyncStream.php
@@ -0,0 +1,90 @@
+<?php
+namespace Net;
+use \plibv4\process\Task;
+use \Net\ProtocolAsync;
+class AsyncStream implements Task {
+	private $socket;
+	private ProtocolAsync $protocol;
+	private bool $terminated = false;
+	function __construct($socket) {
+		$this->socket = $socket;
+		stream_set_blocking($this->socket, false);
+	}
+	
+	public function setProtocol(ProtocolAsync $protocol) {
+		$this->protocol = $protocol;
+	}
+	
+
+	public function __tsError(\Exception $e, int $step): void {
+		echo $e->getMessage().PHP_EOL;
+		fclose($this->socket);
+	}
+
+	public function __tsFinish(): void {
+		fclose($this->socket);
+		echo "Socket closed.".PHP_EOL;
+	}
+
+	public function __tsKill(): void {
+		fclose($this->socket);
+	}
+
+	public function __tsLoop(): bool {
+		if($this->protocol->hasWrite()) {
+			$write = $this->protocol->onWrite();
+			$written = fwrite($this->socket, $write);
+			$this->protocol->onWritten();
+		return true;
+		}
+		/*
+		 * Do not read data if terminated. Just send out what's left.
+		 */
+		if($this->terminated) {
+			return true;
+		}
+		/**
+		 * stream_select cannot be used here, as it will block the script.
+		 * Therefore, instead we'll read and check if we got something.
+		 */
+		#if(@stream_select($readArray, $write, $except, 5, 0)<1) {
+		#	return true;
+		#}
+		$data = fread($this->socket, $this->protocol->getPacketLength());
+		$this->bytes = strlen($this->protocol->getPacketLength());
+		if(feof($this->socket)) {
+			throw new \Exception("Connection closed.");
+		}
+		if($data === "") {
+			return true;
+		}
+		if($data === false) {
+			return true;
+		}
+		$this->protocol->onRead($data);
+	return true;
+	}
+
+	public function __tsPause(): void {
+		
+	}
+
+	public function __tsResume(): void {
+		
+	}
+
+	public function __tsStart(): void {
+		
+	}
+
+	public function __tsTerminate(): bool {
+		/*
+		 * Do not terminate as long Protocol has data left in buffer.
+		 */
+		if($this->protocol->hasWrite()) {
+			return false;
+		}
+		fclose($this->socket);
+	return true;
+	}
+}
