acd92726b0b1ef35d606a40f91b2572a85c35496
Claus-Christoph Küthe <github@vm01.telton.de>
2024-12-02 20:42:32 +0100

    Some types added, but some beautification too.

--- src/include/InEx.php
+++ src/include/InEx.php
@@ -5,82 +5,78 @@
  */
 
 class InEx {
-	private $exclude = array();
-	private $include = array();
-	private $depth = 0;
-	private $convert;
+	private array $exclude = array();
+	private array $include = array();
+	private int $depth = 0;
+	private \ConvertTrailingSlash $convert;
 	function __construct() {
 		$this->convert = new ConvertTrailingSlash(ConvertTrailingSlash::REMOVE);;
 	}
 	
-	function addExclude(string $exclude) {
+	function addExclude(string $exclude): void {
 		$this->exclude[] = $this->convert->convert($exclude);
 	}
 	
-	function addInclude(string $include) {
+	function addInclude(string $include): void {
 		$this->include[] = $this->convert->convert($include);
 	}
 	
-	function isIncluded(string $path) {
+	function isIncluded(string $path): bool {
 		if(empty($this->include)) {
-			return TRUE;
+			return true;
 		}
-		$path = $this->convert->convert($path);
-		foreach($this->include as $key => $value) {
-			if(preg_match("/^". preg_quote($value, "/")."/", $path)) {
+		$sanitizedPath = $this->convert->convert($path);
+		foreach($this->include as $value) {
+			if(preg_match("/^". preg_quote($value, "/")."/", $sanitizedPath)) {
 				return true;
 			}
 		}
-	return FALSE;
+	return false;
 	}
 	
 	function isExcluded(string $path): bool {
 		if(empty($this->exclude)) {
-			return FALSE;
+			return false;
 		}
-		$path = $this->convert->convert($path);
-		foreach($this->exclude as $key => $value) {
-			if(preg_match("/^". preg_quote($value, "/")."/", $path)) {
+		$sanitizedPath = $this->convert->convert($path);
+		foreach($this->exclude as $value) {
+			if(preg_match("/^". preg_quote($value, "/")."/", $sanitizedPath)) {
 				return true;
 			}
 		}
-	return FALSE;
+	return false;
 	}
 	
 	/**
 	 * If a certain path is included, we must be allowed to transit all directories
 	 * to reach that path, but we must not backup their contents.
-	 * @param type $path
-	 * @return boolean
+	 * @param string $path
+	 * @return bool
 	 */
-	function transitOnly($path) {
-		foreach($this->include as $key => $value) {
+	function transitOnly(string $path): bool {
+		foreach($this->include as $value) {
 			if($path==$value) {
-				return FALSE;
+				return false;
 			}
 			if(preg_match("/^". preg_quote($path, "/")."/", $value)) {
 				return true;
 			}
 		}
-	return FALSE;
+	return false;
 	}
 	
-	function isValid($path) {
+	function isValid(string $path): bool {
 		$included = $this->isIncluded($path);
 		$excluded = $this->isExcluded($path);
 		#echo $path.PHP_EOL;
 		#echo "Ex: ".$excluded.PHP_EOL;
 		#echo "In: ".$included.PHP_EOL;
-		if($included == FALSE) {
-			return FALSE;
+		if($included === false) {
+			return false;
 		}
-		if($included == TRUE and $excluded == TRUE) {
-			return FALSE;
+		if($excluded === true) {
+			return false;
 		}
-		if($included == TRUE and $excluded == TRUE) {
-			return FALSE;
-		}
-
-	return TRUE;
+	return true;
 	}
 }
