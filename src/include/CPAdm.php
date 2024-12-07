206dfa4158de33a1876febd30497b0884e046346
DarthCorvidus <github@vm01.telton.de>
2024-12-07 15:36:42 +0100

    Added some psalm findings & removed obsolete code.
    This is a nice one - when starting with the project, I started with a local
    system. CPAdm was an 'administrative' client working directly on the database.
    It is still used in tests due to its handle command function.
    While deprecated at first glance, it is actually good to have around - I could
    increase test coverage by using it to test server functions without a running
    server.
    For Node command, it should be possible to isolate the node protocols.

--- src/include/CPAdm.php
+++ src/include/CPAdm.php
@@ -1,31 +1,29 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Administrative Console for Crow Protect
+ * Administrative Console for Corviprotect
+ * This seems to be the old version of cpnc, but I still use it in unit tests
+ * because of handleCommand, which works directly on the database. Most likely
+ * from the first concept when Corviprotect was entirely local for getting first
+ * concepts done.
+ * I should move this one to tests.
  *
  * @author Claus-Christoph Küthe
  */
 class CPAdm {
-	private $pdo;
-	private $shared;
-	private $argv;
+	private \EPDO $pdo;
+	/** @var list<string> */
+	private array $argv = array();
 	function __construct(EPDO $pdo, array $argv) {
 		$this->pdo = $pdo;
 		$this->argv = $argv;
 	}
 
-	function printWelcome() {
+	function printWelcome(): void {
 		echo "Crow Protect version less than pre-alpha".PHP_EOL;
 		echo "(c) Claus-Christoph Küthe 2023".PHP_EOL;
 	}
 	
-	function getCommand($input = ""): CommandParser {
+	function getCommand(string $input = ""): CommandParser {
 		while($input=="") {
 			#echo "cpmad> ";
 			$input = readline("cpadm> ");
@@ -35,7 +33,7 @@ class CPAdm {
 	return new CommandParser($input);
 	}
 	
-	function handleCommand(CommandParser $command) {
+	function handleCommand(CommandParser $command): void {
 		if($command->getCommand()=="query") {
 			$query = new QueryHandler($this->pdo, $command);
 			$query->run();
@@ -51,56 +49,10 @@ class CPAdm {
 			$query->run();
 			return;
 		}
-		if($command->getCommand()=="md5") {
-			if($command->getObject()=="") {
-				return;
-			}
-			$storage = Storage::fromName($this->pdo, $command->getObject());
-			$stmt = $this->pdo->prepare("select * from n_version2basic where dst_id = ?");
-			$stmt->execute(array($storage->getId()));
-			foreach($stmt as $key => $value) {
-				$path = $storage->getPathForIdFile($value["nvb_id"]);
-				echo $path.PHP_EOL;
-				echo md5_file($path).PHP_EOL;
-			}
-		return;
-		}
-		if($command->getCommand()=="backup") {
-			if($command->getObject()!="partition") {
-				return;
-			}
-			$model = new CPModelBackup($this->pdo);
-			$command->import($model);
-			$sourcePartition = Partition::fromName($this->pdo, $command->getPositional(0));
-			$targetPartition = $sourcePartition->getCopyPartition();
-			$sourceStorage = Storage::fromId($this->pdo, $sourcePartition->getStorageId());
-			$targetStorage = Storage::fromId($this->pdo, $targetPartition->getStorageId());
-			$versionsSource = $sourceStorage->getStoredVersions($sourcePartition);
-			$versionsTarget = $targetStorage->getStoredVersions($targetPartition);
-			$diff = array_diff($versionsSource, $versionsTarget);
-			$max = count($diff);
-			$i=0;
-			$this->pdo->beginTransaction();
-			foreach($diff as $key => $value) {
-				if($i%100==0) {
-					$this->pdo->commit();
-					$this->pdo->beginTransaction();
-				}
-				if($i%1000==0) {
-					echo "Copied ".$i." of ".$max.PHP_EOL;
-					
-				}
-				$sourceHandle = $sourceStorage->getReadHandle($sourcePartition, $value);
-				$targetStorage->storeFromHandle($targetPartition, $value, $sourceHandle);
-				$i++;
-			}
-			$this->pdo->commit();
-		return;
-		}
 		throw new InvalidArgumentException("Invalid command '".$command->getCommand()."'.");
 	}
 	
-	function run() {
+	function run(): void {
 		$this->printWelcome();
 		if(isset($this->argv[1])) {
 			$command = new CommandParser($this->argv[1]);
