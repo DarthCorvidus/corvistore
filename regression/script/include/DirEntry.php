c1cdc868de20a7b49cb67df439d14406ccc008aa
DarthCorvidus <github@vm01.telton.de>
2024-12-23 21:17:57 +0100

    Don't understand why I did not use File in the first place.
    Added further checks.

--- regression/script/include/DirEntry.php
+++ regression/script/include/DirEntry.php
@@ -1,30 +1,28 @@
 <?php
 namespace Regression;
 class DirEntry {
-	private $sha1;
-	private $path;
-	private $ctime;
-	private $mode;
+	private $sha1 = "";
+	private $file;
 	function __construct(string $path) {
-		$this->path = $path;
-		$stat = stat($path);
-		$this->ctime = $stat["ctime"];
-		$this->mode = $stat["mode"];
-		if(is_file($path)) {
+		$this->file = \File::fromPath($path);
+		if($this->file->getType() === \Catalog::TYPE_FILE) {
 			$this->sha1 = sha1_file($path);
 		}
+	}
+
+	public function getFile(): \File {
+		return $this->file;
 	}
 	
 	public function getChecksum(): string {
 		return $this->sha1;
 	}
 	
-	public function getCtime(): int {
-		return $this->ctime;
+	public function getMtime(): int {
+		return $this->file->getMTime();
 	}
 
 	public function getMode(): int {
-		return $this->mode;
+		return $this->file->getPerms();
 	}
-
 }
--- regression/script/include/Directory.php
+++ regression/script/include/Directory.php
@@ -6,6 +6,8 @@ class Directory {
 	private int $countMissingFiles = 0;
 	private int $countDifferentFiles = 0;
 	private int $countDifferentModes = 0;
+	private int $countDifferentCTime = 0;
+	private int $countDifferentType = 0;
 	function __construct(string $path) {
 		$this->recurse($path, "/");
 		$this->path = $path;
@@ -40,28 +42,46 @@ class Directory {
 		#print_r($this->files);
 		foreach($this->files as $key => $value) {
 			if(!isset($directory->files[$key])) {
-				echo "\t".$key." does not exist other directory.".PHP_EOL;
+				#echo "\t".$key." does not exist other directory.".PHP_EOL;
 				$this->countMissingFiles++;
 				$ret = FALSE;
 				continue;
 			}
 			$ours = $this->files[$key];
 			$theirs = $directory->files[$key];
+			if($ours->getFile()->getType() !== $theirs->getFile()->getType()) {
+				$this->countDifferentType++;
+			// It is pointless to check for other differences if type is different.
+			continue;
+			}
+			
 			if($ours->getChecksum()!=$theirs->getChecksum()) {
-				echo "\t".$key." checksum not equal.".PHP_EOL;
+				#echo "\t".$key." checksum not equal.".PHP_EOL;
 				$this->countDifferentFiles++;
 				$ret = FALSE;
 			}
 			if($ours->getMode()!=$theirs->getMode()) {
-				echo "\t".$key." mode not equal.".PHP_EOL;
+				#echo "\t".$key." mode not equal.".PHP_EOL;
 				$this->countDifferentModes++;
+				$ret = FALSE;
+			}
+			
+			if($ours->getFile()->getType() === \Catalog::TYPE_LINK) {
+				continue;
+			}
+			
+			if($ours->getMtime()!=$theirs->getMtime()) {
+				echo "Different ctime for ".$key.PHP_EOL;
+				$this->countDifferentCTime++;
 				$ret = FALSE;
 			}
 		}
 		echo "Compared ".$this->path." vs ".$directory->path.":".PHP_EOL;
-		echo "\tMissing files: ".$this->countMissingFiles.PHP_EOL;
-		echo "\tDifferent files: ".$this->countDifferentFiles.PHP_EOL;
-		echo "\tDifferent modes: ".$this->countDifferentModes.PHP_EOL;
+		echo "\tMissing file: ".$this->countMissingFiles.PHP_EOL;
+		echo "\tDifferent file: ".$this->countDifferentFiles.PHP_EOL;
+		echo "\tDifferent mode: ".$this->countDifferentModes.PHP_EOL;
+		echo "\tDifferent ctime: ".$this->countDifferentCTime.PHP_EOL;
+		echo "\tDifferent type: ".$this->countDifferentType.PHP_EOL;
 	return $ret;
 	}
 }
