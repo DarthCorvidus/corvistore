a014f1dc8510ba11eb6249d2c840db4178abcfe1
DarthCorvidus <github@vm01.telton.de>
2024-12-25 16:08:34 +0100

    Removed some dead code.

--- src/include/Net/ProtocolListener.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Net;
-/**
- * @deprecated since version 0.0.1
- */
-interface ProtocolListener {
-	function onQuit(): void;
-	function onCommand(string $data, Protocol $protocol): void;
-}
