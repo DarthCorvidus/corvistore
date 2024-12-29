57a723855dd5c11854bc881bfced2930cc645ed3
DarthCorvidus <github@vm01.telton.de>
2024-12-29 14:48:10 +0100

    Increased time stamp binary representation to 64 bit integers
    The only place where I actually use BinStruct and I get it wrong ;-).

--- src/include/BinStruct/VersionEntry.php
+++ src/include/BinStruct/VersionEntry.php
@@ -5,15 +5,15 @@ class VersionEntry implements \BinStruct {
 	private array $values = array();
 	function __construct() {
 		$this->values["dvs_id"] = \IntVal::uint64LE();
-		$this->values["dvs_mtime"] = \IntVal::uint32LE();
+		$this->values["dvs_mtime"] = \IntVal::uint64LE();
 		$this->values["dvs_permissions"] = \IntVal::uint16LE();
 		$this->values["dvs_owner"] = new \PrefixedStringVal(\IntVal::uint8());
 		$this->values["dvs_group"] = new \PrefixedStringVal(\IntVal::uint8());
-		$this->values["dvs_size"] = \IntVal::uint32LE();
-		$this->values["dvs_created_epoch"] = \IntVal::uint32LE();
+		$this->values["dvs_size"] = \IntVal::uint64LE();
+		$this->values["dvs_created_epoch"] = \IntVal::uint64LE();
 		$this->values["dvs_type"] = \IntVal::uint8();
 		$this->values["dvs_stored"] = \IntVal::uint8();
-		$this->values["dc_id"] = \IntVal::uint8();
+		$this->values["dc_id"] = \IntVal::uint64LE();
 	}
 
 	public function getBinStruct(string $name): \BinStruct {
